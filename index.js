fe44ae62e317e74dbf13cd9907077a8ffce2e318
minyoungshin <mins6649@gmail.com>
2023-02-02 13:48:29 -0500

    array methods: push,unshift,pop,shift

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCats = cats.slice();
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name){
+    const newCats = cats.slice();
+    newCats.unshift(name);
+    return newCats;
+}
+function removeLastCat(){
+    const newCats = cats.slice();
+    newCats.pop();
+    return newCats;   
+}
+function removeFirstCat(){
+    const newCats = cats.slice();
+    newCats.shift();
+    return newCats;   
+}
